d18cd5c19df26d030e2e1b5652c085ed4062e9fc
九级浪 <413612464@qq.com>
2023-08-07 17:51:12 +0800

    feat:光线投射实现场景交互

--- src/views/BaseView/13.加载gltf模型_加载压缩过的模型.vue
+++ src/views/BaseView/13.加载gltf模型_加载压缩过的模型.vue
@@ -1,4 +1,4 @@
-<!-- 14.光线投射实现场景交互 -->
+<!-- 13.加载gltf模型_加载压缩过的模型 -->
 <template>
   <div class="container" ref="container"></div>
 </template>
@@ -21,52 +21,36 @@ const scene = new THREE.Scene();
 
 // 1.2 创建相机
 const camera = new THREE.PerspectiveCamera(75, innerWidth / innerHeight, 0.1, 1000);
-camera.position.set(1, 2, 15);
+camera.position.set(1, 2, 3);
 scene.add(camera);
 
-// 14.1 创建球
-function createSphere(color) {
-  return (new THREE.Mesh(
-    new THREE.SphereGeometry(1, 32, 32),
-    new THREE.MeshBasicMaterial({
-      color
-    })
-  ));
-}
-const sphere1 = createSphere(0x00ff00);
-const sphere2 = createSphere(0x0000ff);
-const sphere3 = createSphere(0xff00ff);
-sphere1.position.x = -4;
-sphere2.position.x = 0;
-sphere3.position.x = 4;
-scene.add(sphere1);
-scene.add(sphere2);
-scene.add(sphere3);
-// 14.2 创建射线
-const raycaster = new THREE.Raycaster();
-// 14.3 创建鼠标向量，用来保存鼠标点在画面哪里
-const mouse = new THREE.Vector2();
-// 14.4 监听点击事件
-document.addEventListener('click', event => {
-  // 14.4.1 设置鼠标向量经x,y的值
-  mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
-  mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
-  // console.log(mouse.x, mouse.y);
-  // 14.4.2 通过摄像机和鼠标位置更新射线
-  raycaster.setFromCamera(mouse, camera);
-  // 14.4.3 检测射线有没与物体相交
-  const intersects = raycaster.intersectObjects([sphere1, sphere2, sphere3]);
-  // 14.4.4 如果有相交的物体，取最近的一个，改变其颜色
-  if (intersects.length > 0) {
-    const [firstSphere] = intersects;
-    if (firstSphere.object._isSelect) {
-      firstSphere.object.material.color.set(firstSphere.object._originColor);
-    } else {
-      firstSphere.object._originColor = firstSphere.object.material.color.getHex();
-      firstSphere.object.material.color.set(0xff0000);
-    }
-    firstSphere.object._isSelect = !firstSphere.object._isSelect;
-  }
+// 12.3 创建指数雾
+scene.fog = new THREE.FogExp2(0x999999, 0.1);
+// 12.4 设置场景背景与雾同色
+scene.background = new THREE.Color(0x999999);
+
+// 13.1 实例化加载器gltf
+const gltfLoader = new GLTFLoader();
+// 13.2 加载鸭子模型
+gltfLoader.load('./model/Duck.glb', gltf => { // glb: 二进制数据，gltf: json数据
+  scene.add(gltf.scene);
+});
+// 13.4 加载城市模型
+// 13.4.1 实例化加载器draco
+const dracoLoader = new DRACOLoader();
+// 13.4.2 设置draco路径
+dracoLoader.setDecoderPath('./draco/');
+// 13.4.3 为gltf加载器设置draco解码器
+gltfLoader.setDRACOLoader(dracoLoader);
+// 13.4.4 加载压缩过的城市模型
+gltfLoader.load('./model/city.glb', gltf => { // glb: 二进制数据，gltf: json数据
+  scene.add(gltf.scene);
+});
+// 13.3 加载环境贴图
+const rgbeLoader = new RGBELoader();
+rgbeLoader.load('./texture/Alex_Hart-Nature_Lab_Bones_2k.hdr', envMap => {
+  envMap.mapping = THREE.EquirectangularReflectionMapping;
+  scene.environment = envMap;
 });
 
 const gui = new GUI();

879097ae4eaa4472be290af92f16290584e2edb4
九级浪 <413612464@qq.com>
2023-08-07 14:30:25 +0800

    feat:加载gltf模型_加载压缩过的模型

--- src/views/BaseView/12.雾.vue
+++ src/views/BaseView/12.雾.vue
@@ -1,4 +1,4 @@
-<!-- 13.加载gltf模型_加载压缩过的模型 -->
+<!-- 12.雾 -->
 <template>
   <div class="container" ref="container"></div>
 </template>
@@ -9,8 +9,6 @@ import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 import { GUI } from 'three/examples/jsm/libs/lil-gui.module.min.js';
 import { RGBELoader } from 'three/examples/jsm/loaders/RGBELoader';
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
-import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader';
 
 let innerWidth = window.innerWidth;
 let innerHeight = window.innerHeight;
@@ -21,37 +19,22 @@ const scene = new THREE.Scene();
 
 // 1.2 创建相机
 const camera = new THREE.PerspectiveCamera(75, innerWidth / innerHeight, 0.1, 1000);
-camera.position.set(1, 2, 3);
+camera.position.set(2, 5, 10);
 scene.add(camera);
 
+// 12.1 创建物体
+const boxGeometry = new THREE.BoxGeometry(1, 1, 50);
+const boxMaterial = new THREE.MeshBasicMaterial({
+  color: 0x00ff00,
+});
+const box = new THREE.Mesh(boxGeometry, boxMaterial);
+scene.add(box);
+// 12.2 创建线性雾
+// scene.fog = new THREE.Fog(0x999999, 0.1, 50);
 // 12.3 创建指数雾
 scene.fog = new THREE.FogExp2(0x999999, 0.1);
 // 12.4 设置场景背景与雾同色
 scene.background = new THREE.Color(0x999999);
-
-// 13.1 实例化加载器gltf
-const gltfLoader = new GLTFLoader();
-// 13.2 加载鸭子模型
-gltfLoader.load('./model/Duck.glb', gltf => { // glb: 二进制数据，gltf: json数据
-  scene.add(gltf.scene);
-});
-// 13.4 加载城市模型
-// 13.4.1 实例化加载器draco
-const dracoLoader = new DRACOLoader();
-// 13.4.2 设置draco路径
-dracoLoader.setDecoderPath('./draco/');
-// 13.4.3 为gltf加载器设置draco解码器
-gltfLoader.setDRACOLoader(dracoLoader);
-// 13.4.4 加载压缩过的城市模型
-gltfLoader.load('./model/city.glb', gltf => { // glb: 二进制数据，gltf: json数据
-  scene.add(gltf.scene);
-});
-// 13.3 加载环境贴图
-const rgbeLoader = new RGBELoader();
-rgbeLoader.load('./texture/Alex_Hart-Nature_Lab_Bones_2k.hdr', envMap => {
-  envMap.mapping = THREE.EquirectangularReflectionMapping;
-  scene.environment = envMap;
-});
 
 const gui = new GUI();
 

b6dd59dc5797f132c9f7e46a77ec1b099aefe9bc
九级浪 <413612464@qq.com>
2023-08-08 15:12:31 +0800

    feat:材质捕获贴图

---
+++ src/views/BaseView4/BaseView.vue
@@ -0,0 +1,98 @@
+<!-- 25.材质捕获贴图 -->
+<template>
+  <div class="container" ref="container"></div>
+</template>
+
+<script setup>
+import { ref, onMounted } from 'vue';
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import { GUI } from 'three/examples/jsm/libs/lil-gui.module.min.js';
+import { RGBELoader } from 'three/examples/jsm/loaders/RGBELoader';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
+import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader';
+import * as TWEEN from 'three/examples/jsm/libs/tween.module.js';
+import { VertexNormalsHelper } from 'three/examples/jsm/helpers/VertexNormalsHelper';
+import TheCar from '../../components/TheCar.vue';
+import { TextureLoader } from 'three';
+
+let innerWidth = window.innerWidth;
+let innerHeight = window.innerHeight;
+let container = ref(null);
+
+// 1.1 创建场景
+const scene = new THREE.Scene();
+
+// 1.2 创建相机
+const camera = new THREE.PerspectiveCamera(75, innerWidth / innerHeight, 0.1, 1000);
+camera.position.set(1, 2, 5);
+scene.add(camera);
+
+// 25.1 加载模型【13.1 实例化加载器gltf】
+const gltfLoader = new GLTFLoader();
+gltfLoader.load('./model/Duck.glb', gltf => {
+  scene.add(gltf.scene);
+  const duckMesh = gltf.scene.getObjectByName('LOD3spShape');
+  // 25.2 加载贴图
+  const matcapTexture = new TextureLoader().load('./texture/matcaps/9.jpg');
+  // 25.4 保存源材质
+  const sourceMaterial = duckMesh.material;
+  // 25.3 设置材质为贴图的材质
+  duckMesh.material = new THREE.MeshMatcapMaterial({
+    matcap: matcapTexture,
+    // 25.5 加上原来的材质
+    map: sourceMaterial.map
+  });
+});
+
+// 1.4 创建渲染器
+const renderer = new THREE.WebGLRenderer({ antialias: true });
+renderer.setSize(innerWidth, innerHeight);
+
+// 1.6 创建控制器
+let cantrols = null;
+function createControls() {
+  cantrols = new OrbitControls(camera, container.value);
+  cantrols.enableDamping = true;
+  cantrols.dampingFactor = 0.05; // 阻尼：值越大惯性越小，0不能动
+
+  container.value.appendChild(renderer.domElement);
+}
+
+// 1.7 添加坐标轴辅助器
+const axesHelper = new THREE.AxesHelper(5);
+scene.add(axesHelper);
+
+// 1.5 创建渲染函数
+function render() {
+  cantrols && cantrols.update();
+
+  renderer.render(scene, camera);
+  requestAnimationFrame(render);
+};
+
+// 4.1 监听视口变化
+window.addEventListener('resize', () => {
+  const innerWidth = window.innerWidth;
+  const innerHeight = window.innerHeight;
+  // 更新渲染器大小
+  renderer.setSize(innerWidth, innerHeight);
+  // 更新摄像头宽高比
+  camera.aspect = innerWidth / innerHeight;
+  // 更新摄像头投影矩阵
+  camera.updateProjectionMatrix();
+});
+
+onMounted(() => {
+  createControls();
+  render();
+});
+</script>
+
+<style scope>
+.container {
+  width: 100vw;
+  height: 100vh;
+  background-color: #f0f0f0;
+}
+</style>

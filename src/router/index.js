babdd88765bf22aba075f81082fb8e3a53734664
九级浪 <413612464@qq.com>
2023-08-11 13:46:28 +0800

    feat:Points与点材质

--- src/router/index.js
+++ src/router/index.js
@@ -40,7 +40,7 @@ const router = createRouter({
     {
       path: '/baseView',
       name: 'baseView',
-      component: () => import('../views/BaseView6/BaseView.vue')
+      component: () => import('../views/BaseView7/BaseView.vue')
     },
   ]
 });
---
+++ src/views/BaseView7/BaseView.vue
@@ -0,0 +1,109 @@
+<!-- 60.Points与点材质 -->
+<template>
+  <div class="container" ref="container"></div>
+</template>
+
+<script setup>
+import { ref, onMounted } from 'vue';
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import { GUI } from 'three/examples/jsm/libs/lil-gui.module.min.js';
+import { RGBELoader } from 'three/examples/jsm/loaders/RGBELoader';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
+import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader';
+import { KTX2Loader } from 'three/examples/jsm/loaders/KTX2Loader';
+import { DDSLoader } from 'three/examples/jsm/loaders/DDSLoader';
+import { TGALoader } from 'three/examples/jsm/loaders/TGALoader';
+import { EXRLoader } from 'three/examples/jsm/loaders/EXRLoader';
+import { LogLuvLoader } from 'three/examples/jsm/loaders/LogLuvLoader';
+import { RGBMLoader } from 'three/examples/jsm/loaders/RGBMLoader';
+import * as TWEEN from 'three/examples/jsm/libs/tween.module.js';
+import gsap from 'gsap';
+
+let innerWidth = window.innerWidth;
+let innerHeight = window.innerHeight;
+let container = ref(null);
+
+// 1.1 创建场景
+const scene = new THREE.Scene();
+
+// 1.2 创建相机
+const camera = new THREE.PerspectiveCamera(75, innerWidth / innerHeight, 0.1, 1000);
+camera.position.set(1, 3, 5);
+scene.add(camera);
+
+// 1.4 创建渲染器
+const renderer = new THREE.WebGLRenderer({ antialias: true });
+renderer.setSize(innerWidth, innerHeight);
+
+// 10.1 创建RGBELoader
+const rgbeLoader = new RGBELoader();
+// 10.2 加载hdr图
+rgbeLoader.load('./texture/Alex_Hart-Nature_Lab_Bones_2k.hdr', envMap => {
+  envMap.mapping = THREE.EquirectangularReflectionMapping;
+  scene.background = envMap;
+  scene.environment = envMap;
+});
+
+const gui = new GUI();
+
+// 60.1 创建球几何体
+const spheregeometry = new THREE.SphereGeometry(3, 20, 20);
+// 60.2 创建点材质
+const pointsMaterial = new THREE.PointsMaterial();
+// 60.3 设置点材质的大小
+pointsMaterial.size = 0.1;
+// 60.4 创建点物体
+const points = new THREE.Points(spheregeometry, pointsMaterial);
+scene.add(points);
+
+// 1.6 创建控制器
+let cantrols = null;
+function createControls() {
+  cantrols = new OrbitControls(camera, container.value);
+  cantrols.enableDamping = true;
+  cantrols.dampingFactor = 0.05; // 阻尼：值越大惯性越小，0不能动
+
+  container.value.appendChild(renderer.domElement);
+}
+
+// 1.7 添加坐标轴辅助器
+const axesHelper = new THREE.AxesHelper(5);
+scene.add(axesHelper);
+
+// 设置时钟
+const clock = new THREE.Clock();
+
+// 1.5 创建渲染函数
+function render() {
+  const time = clock.getElapsedTime();
+  cantrols && cantrols.update();
+  renderer.render(scene, camera);
+  requestAnimationFrame(render);
+};
+
+// 4.1 监听视口变化
+window.addEventListener('resize', () => {
+  const innerWidth = window.innerWidth;
+  const innerHeight = window.innerHeight;
+  // 更新渲染器大小
+  renderer.setSize(innerWidth, innerHeight);
+  // 更新摄像头宽高比
+  camera.aspect = innerWidth / innerHeight;
+  // 更新摄像头投影矩阵
+  camera.updateProjectionMatrix();
+});
+
+onMounted(() => {
+  createControls();
+  render();
+});
+</script>
+
+<style scope>
+.container {
+  width: 100vw;
+  height: 100vh;
+  background-color: #f0f0f0;
+}
+</style>

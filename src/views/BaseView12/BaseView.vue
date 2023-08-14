ad3b3b6792a00b98ef1545d7e964cb1f19dd16ed
九级浪 <413612464@qq.com>
2023-08-14 15:50:00 +0800

    feat:初识着色器语言

---
+++ src/views/BaseView12/BaseView.vue
@@ -0,0 +1,116 @@
+<!-- 83.初识着色器语言 -->
+<template>
+  <div class="container" ref="container"></div>
+</template>
+
+<script setup>
+import { ref, onMounted } from 'vue';
+
+import gsap from 'gsap';
+import * as THREE from 'three';
+import * as CANNON from 'cannon-es';
+import * as TWEEN from 'three/examples/jsm/libs/tween.module.js';
+
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import { GUI } from 'three/examples/jsm/libs/lil-gui.module.min.js';
+import { RGBELoader } from 'three/examples/jsm/loaders/RGBELoader';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
+import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader';
+import { KTX2Loader } from 'three/examples/jsm/loaders/KTX2Loader';
+import { DDSLoader } from 'three/examples/jsm/loaders/DDSLoader';
+import { TGALoader } from 'three/examples/jsm/loaders/TGALoader';
+import { EXRLoader } from 'three/examples/jsm/loaders/EXRLoader';
+import { LogLuvLoader } from 'three/examples/jsm/loaders/LogLuvLoader';
+import { RGBMLoader } from 'three/examples/jsm/loaders/RGBMLoader';
+
+
+let innerWidth = window.innerWidth;
+let innerHeight = window.innerHeight;
+let container = ref(null);
+
+// 1.1 创建场景
+const scene = new THREE.Scene();
+
+// 1.2 创建相机
+const camera = new THREE.PerspectiveCamera(75, innerWidth / innerHeight, 0.1, 300);
+camera.position.set(1, 2, 5);
+scene.add(camera);
+
+const gui = new GUI();
+
+
+// 83.2 创建着色器材质
+const shaderMaterial = new THREE.ShaderMaterial({
+  // 83.2.1 顶点着色器
+  vertexShader: `
+    void main() {
+      gl_Position = projectionMatrix * viewMatrix * modelMatrix * vec4(position, 1.0);
+    }
+  `,
+  // 83.2.2 片元着色器
+  fragmentShader: `
+    void main() {
+      gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
+    }
+  `,
+});
+// 83.1 创建平面
+const plane = new THREE.Mesh(
+  new THREE.PlaneGeometry(1, 1, 64, 64),
+  shaderMaterial
+);
+scene.add(plane);
+
+// 1.4 创建渲染器
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(innerWidth, innerHeight);
+// 75.2.2 允许在场景中使用阴影贴图
+renderer.shadowMap.enabled = true;
+
+// 1.6 创建控制器
+let cantrols = null;
+function createControls() {
+  cantrols = new OrbitControls(camera, container.value);
+  cantrols.enableDamping = true;
+  cantrols.dampingFactor = 0.05; // 阻尼：值越大惯性越小，0不能动
+}
+
+// 1.7 添加坐标轴辅助器
+const axesHelper = new THREE.AxesHelper(5);
+scene.add(axesHelper);
+
+// 设置时钟
+const clock = new THREE.Clock();
+
+// 1.5 创建渲染函数
+function render() {
+  cantrols && cantrols.update();
+  renderer.render(scene, camera);
+  requestAnimationFrame(render);
+};
+
+// 4.1 监听视口变化
+window.addEventListener('resize', () => {
+  const innerWidth = window.innerWidth;
+  const innerHeight = window.innerHeight;
+  // 更新渲染器大小
+  renderer.setSize(innerWidth, innerHeight);
+  // 更新摄像头宽高比
+  camera.aspect = innerWidth / innerHeight;
+  // 更新摄像头投影矩阵
+  camera.updateProjectionMatrix();
+});
+
+onMounted(() => {
+  container.value.appendChild(renderer.domElement);
+  createControls();
+  render();
+});
+</script>
+
+<style lang="less" scope>
+.container {
+  width: 100vw;
+  height: 100vh;
+}
+</style>

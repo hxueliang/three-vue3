5ba7b30e2ac78a85f16372dc1fa5877b825738af
九级浪 <413612464@qq.com>
2023-08-17 17:04:52 +0800

    feat:点着色器设置不同的图形_初始化

--- src/views/BaseView14/93.点着色器图形设置.vue
+++ src/views/BaseView14/93.点着色器图形设置.vue
@@ -1,4 +1,4 @@
-<!-- 94.点着色器设置不同的图形 -->
+<!-- 93.点着色器图形设置 -->
 <template>
   <div class="container" ref="container"></div>
 </template>
@@ -36,95 +36,40 @@ const scene = new THREE.Scene();
 
 // 1.2 创建相机
 const camera = new THREE.PerspectiveCamera(75, innerWidth / innerHeight, 0.1, 300);
-camera.position.set(1, 2, 5);
+camera.position.set(1, 2, 40);
 scene.add(camera);
 
 const gui = new GUI();
+
+// 92.1 创建几何体
+const geometry = new THREE.BufferGeometry();
+const positions = new Float32Array([0, 0, 0]);
+geometry.setAttribute('position', new THREE.BufferAttribute(positions, 3));
 
 // 92.3.1 加载纹理
 const textureLoader = new THREE.TextureLoader();
 const texture = textureLoader.load('./texture/particles/1.png');
 
-// 64.1 初始化数据
-const params = {
-  count: 50000,
-  size: 0.1,
-  radius: 5,
-  branch: 4,
-  // 67.1 设置圆心为亮红色，边缘为亮蓝色，用于从红色过渡到蓝色
-  color: '#ff6030',
-  endColor: '#1b3984',
-  bend: 0.3, // 控制弯曲程度
-};
-
-// 67.3 初始化中心颜色 边缘颜色
-const centerColor = new THREE.Color(params.color);
-const endColor = new THREE.Color(params.endColor);
-
-// 94.1.0 定义全局变量
-let geometry = null;
-let material = null;
-let points = null;
-
-// 64.2 创建星系
-function createGalaxy() {
-
-  // 94.1.1 如果已经存在这些顶点，先释放内存，在删除顶点数据
-  if (points) {
-    geometry.dispose();
-    material.dispose();
-    scene.remove(points);
-  }
-
-  geometry = new THREE.BufferGeometry();
-  const positions = new Float32Array(params.count * 3);
-  // 67.2 创建顶点颜色数组
-  const colors = new Float32Array(params.count * 3);
-  for (let i = 0; i < params.count; i++) {
-    // 65.1 分支角度 = 第几条分支 * 角度
-    const barnch = i % params.branch;
-    const angle = 2 * Math.PI / params.branch;
-    const barnchAngle = barnch * angle;
-    // 66.4 0到1再做3次方,实现圆心密集【65.2 当前点距离圆心的位置】
-    const r = Math.random() * params.radius * Math.pow(Math.random(), 3);
-    const current = i * 3;
-    // 66.5 乘上(radius-r)*0.2,实现圆心厚边缘薄【66.3.1 -1到1再做3次方,实现线中心密集】【66.2.1 让点在曲线附近散开】
-    const randomX = Math.pow(Math.random() * 2 - 1, 3) * (params.radius - r) * 0.2;
-    const randomY = Math.pow(Math.random() * 2 - 1, 3) * (params.radius - r) * 0.2;
-    const randomZ = Math.pow(Math.random() * 2 - 1, 3) * (params.radius - r) * 0.2;
-    // 66.2.2 加上随机值【66.1.1 角度值加上距离实现直线弯曲】【65.3.1 修改x值】
-    positions[current] = Math.cos(barnchAngle + r * params.bend) * r + randomX;
-    // 66.2.3 加上随机值
-    positions[current + 1] = 0 + randomY;
-    // 66.2.4 加上随机值 【66.1.1 角度值加上距离实现直线弯曲】【65.3.2 修改y值】
-    positions[current + 2] = Math.sin(barnchAngle + r * params.bend) * r + randomZ;
-    // 67.4 混合颜色，形成渐变色
-    const mixColor = centerColor.clone();
-    mixColor.lerp(endColor, r / params.radius);
-    colors[current] = mixColor.r;
-    colors[current + 1] = mixColor.g;
-    colors[current + 2] = mixColor.b;
-  }
-  geometry.setAttribute('position', new THREE.BufferAttribute(positions, 3));
-  // 67.6 设置颜色属性
-  geometry.setAttribute('color', new THREE.BufferAttribute(colors, 3));
-
-  // 94.1.2 创建材质
-  material = new THREE.ShaderMaterial({
-    vertexShader,
-    fragmentShader,
-    transparent: true,
-    uniforms: {
-      uTexture: {
-        value: texture
-      }
+// 92.2.1 创建点材质
+// const material = new THREE.PointsMaterial({
+//   color: 0xff0000,
+//   size: 10,
+//   sizeAttenuation: true,
+// });
+// 92.2.2 创建点着色器材质
+const material = new THREE.ShaderMaterial({
+  vertexShader,
+  fragmentShader,
+  transparent: true,
+  uniforms: {
+    uTexture: {
+      value: texture
     }
-  });
-
-  points = new THREE.Points(geometry, material);
-  scene.add(points);
-}
-createGalaxy();
+  }
+});
+// 92.3 生成点
+const points = new THREE.Points(geometry, material);
+scene.add(points);
 
 // 1.4 创建渲染器
 const renderer = new THREE.WebGLRenderer({ alpha: true });

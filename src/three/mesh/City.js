ef91c95db5ff7cff0e22e74681dc87a06a721d2a
九级浪 <413612464@qq.com>
2023-08-24 16:23:43 +0800

    feat:给模型添加渐变色

--- src/three/mesh/City.js
+++ src/three/mesh/City.js
@@ -1,19 +1,20 @@
 import * as THREE from 'three';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 import scene from '../scene';
+import modifyCityMaterial from '../modify/modifyCityMaterial';
 
 
 export default function createCity() {
   const gltfLoader = new GLTFLoader();
   const material = new THREE.MeshBasicMaterial({
-    color: 0xff00ff
+    color: 0x0c0c6f
   });
   gltfLoader.load('./model/city/city.glb', gltf => {
-    console.log(gltf);
     const gltfScene = gltf.scene;
     gltfScene.traverse(item => {
       if (item.type === 'Mesh') {
         item.material = material;
+        modifyCityMaterial(item);
       }
     });
     scene.add(gltfScene);
---
+++ src/three/modify/modifyCityMaterial.js
@@ -0,0 +1,50 @@
+import * as THREE from 'three';
+
+export default function modifyCityMaterial(mesh) {
+  mesh.material.onBeforeCompile = shader => {
+    console.log(shader.vertexShader);
+    console.log(shader.fragmentShader);
+    addGradualColor(shader, mesh);
+  };
+}
+
+// 给模型添加渐变色
+function addGradualColor(shader, mesh) {
+  const { geometry } = mesh;
+  geometry.computeBoundingBox();
+  const { max, min } = geometry.boundingBox;
+  const uHeight = max.y - min.y;
+
+  shader.uniforms.uHeight = { value: uHeight };
+  shader.uniforms.uTopColor = { value: new THREE.Color(0xaaaaff) };
+  shader.vertexShader = shader.vertexShader.replace(
+    `#include <common>`,
+    `#include <common>
+      varying vec3 vPosition;
+      `
+  );
+  shader.vertexShader = shader.vertexShader.replace(
+    `#include <begin_vertex>`,
+    `#include <begin_vertex>
+      vPosition = position;
+      `
+  );
+  shader.fragmentShader = shader.fragmentShader.replace(
+    `#include <common>`,
+    `#include <common>
+      varying vec3 vPosition;
+      uniform float uHeight;
+      uniform vec3 uTopColor;
+      `
+  );
+  shader.fragmentShader = shader.fragmentShader.replace(
+    `#include <dithering_fragment>`,
+    `#include <dithering_fragment>
+      vec4 distGradualColor = gl_FragColor;
+      float gradualMix = (vPosition.y + uHeight / 2.0) / uHeight;
+      vec3 gradualColor = mix(distGradualColor.xyz, uTopColor, gradualMix);
+
+      gl_FragColor = vec4(gradualColor, 1);
+      `
+  );
+}

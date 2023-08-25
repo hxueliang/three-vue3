4d832b4ebb64a986ae7d314fc632d3c09913c482
九级浪 <413612464@qq.com>
2023-08-25 18:59:33 +0800

    chore:配置require插件

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,16 @@ import { fileURLToPath, URL } from 'node:url';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import glsl from 'vite-plugin-glsl';
+import requireTransform from 'vite-plugin-require-transform';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     glsl(),
+    requireTransform({
+      fileRegex: /.js$|.vue$/
+    })
   ],
   resolve: {
     alias: {

4e61431bb59ef9113e41c4dbb8f4c7d910a18413
九级浪 <413612464@qq.com>
2023-08-30 23:52:26 +0800

    feat:智慧园区_汽车延马路移动

--- src/views/BaseView28/three/camera.js
+++ src/views/BaseView28/three/camera.js
@@ -6,6 +6,6 @@ const camera = new THREE.PerspectiveCamera(
   0.1,
   1000
 );
-camera.position.set(0, 4, 5);
+camera.position.set(4.1, 1, 0.5);
 
 export default camera;
--- src/views/BaseView28/three/mesh/Area.js
+++ src/views/BaseView28/three/mesh/Area.js
@@ -1,5 +1,6 @@
 import * as THREE from 'three';
 import { ref } from 'vue';
+import gsap from "gsap";
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader';
 
@@ -27,11 +28,42 @@ export default class Area {
           this.action.play();
         }
 
+        if (child.name === '汽车园区轨迹') {
+          const line = child;
+          const points = [];
+          // const points2 = [];
+          const { position } = line.geometry.attributes;
+          for (let i = position.count - 1; i >= 0; i--) {
+            points.push(new THREE.Vector3(
+              position.getX(i),
+              position.getY(i),
+              position.getZ(i)
+            ));
+            // points2.push(new THREE.Vector3(
+            //   position.array[i * 3 + 0],
+            //   position.array[i * 3 + 1],
+            //   position.array[i * 3 + 2]
+            // ));
+          }
+
+          // 创建曲线
+          this.curve = new THREE.CatmullRomCurve3(points);
+          // 在曲线的什么位置0~1
+          this.curveProgress = 0;
+          this.carAnimation();
+        }
+
+        if (child.name === '小汽车') {
+          this.car = child;
+        }
+
+        if (child.name === 'NURBS_曲线') {
+          child.visible = false;
+        }
       });
     });
 
     eventHub.on("actionClick", (i) => {
-      console.log(i);
       // this.action.reset();
       this.clip = this.gltf.animations[i];
       this.action = this.mixer.clipAction(this.clip);
@@ -43,5 +75,23 @@ export default class Area {
     if (this.mixer) {
       this.mixer.update(time);
     }
+  }
+
+  carAnimation() {
+    gsap.to(this, {
+      curveProgress: 0.999,
+      duration: 30,
+      repeat: -1,
+      onUpdate: value => {
+        const point = this.curve.getPoint(this.curveProgress);
+        const { x, y, z } = point;
+        this.car.position.set(x, y, z);
+        // 获取下一个点，让小汽车朝向下一个点
+        if (this.curveProgress + 0.001 < 1) {
+          const nextPoint = this.curve.getPoint(this.curveProgress + 0.001);
+          this.car.lookAt(nextPoint);
+        }
+      }
+    });
   }
 }

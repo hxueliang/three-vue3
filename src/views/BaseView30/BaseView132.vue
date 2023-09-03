5cd54eca7514d9c89e44efc26fde472a9d76f303
九级浪 <413612464@qq.com>
2023-09-03 20:22:05 +0800

    feat:分层设置物体发光

--- src/views/BaseView30/BaseView132.vue
+++ src/views/BaseView30/BaseView132.vue
@@ -22,6 +22,10 @@ import { EXRLoader } from 'three/examples/jsm/loaders/EXRLoader';
 import { LogLuvLoader } from 'three/examples/jsm/loaders/LogLuvLoader';
 import { RGBMLoader } from 'three/examples/jsm/loaders/RGBMLoader';
 
+import { EffectComposer } from "three/examples/jsm/postprocessing/EffectComposer.js";
+import { RenderPass } from "three/examples/jsm/postprocessing/RenderPass.js";
+import { UnrealBloomPass } from "three/examples/jsm/postprocessing/UnrealBloomPass.js";
+
 innerWidth = window.innerWidth;
 innerHeight = window.innerHeight;
 const container = ref(null);
@@ -33,6 +37,8 @@ const textureLoader = new THREE.TextureLoader();
 let scene, camera, renderer, controls;
 
 let sphere, torusKnot, cube;
+
+let composer;
 
 init();
 
@@ -53,6 +59,61 @@ function createCode() {
   createSphere();
   createTorusKnot();
   createBox();
+
+  createEffect();
+  initEvent();
+}
+
+// 初始化点击事件
+function initEvent() {
+  window.addEventListener('click', () => {
+    if (sphere.layers.mask === 1) {
+      sphere.layers.set(1);
+    } else {
+      sphere.layers.set(0);
+    }
+  });
+
+  /*
+  const raycaster = new THREE.Raycaster();
+  const mouse = new THREE.Vector2();
+  const meshArr = [sphere, torusKnot, cube];
+  window.addEventListener('click', event => {
+    mouse.x = event.clientX / window.innerWidth * 2 - 1;
+    mouse.y = -(event.clientY / window.innerHeight * 2 - 1);
+    raycaster.setFromCamera(mouse, camera);
+    let intersects = raycaster.intersectObjects(meshArr, false);
+    // Todo: raycaster无法检测不同层的物体
+    if (!intersects[0]) {
+      return;
+    }
+    const mesh = intersects[0].object;
+    if (mesh.layers.mask === 1) {
+      mesh.layers.set(1);
+    } else {
+      mesh.layers.set(0);
+    }
+  });
+  */
+}
+
+// 添加后期处理
+function createEffect() {
+  composer = new EffectComposer(renderer);
+  const renderPass = new RenderPass(scene, camera);
+  composer.addPass(renderPass);
+
+  const unrealBloomPass = new UnrealBloomPass(
+    // 要影响泛光的画布大小
+    new THREE.Vector2(innerWidth, innerHeight),
+    // 泛光的强度
+    1.5,
+    // 泛光的半径
+    0.4,
+    // 泛光的阈值
+    0.2
+  );
+  composer.addPass(unrealBloomPass);
 }
 
 // 创建球
@@ -66,7 +127,7 @@ function createSphere() {
 
 // 创建扭结
 function createTorusKnot() {
-  const geometry = new THREE.TorusKnotGeometry(1, 0.4, 64, 8);
+  const geometry = new THREE.TorusKnotGeometry(1, 0.2, 64, 8);
   const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
   torusKnot = new THREE.Mesh(geometry, material);
   scene.add(torusKnot);
@@ -78,6 +139,10 @@ function createBox() {
   const material = new THREE.MeshBasicMaterial({ color: 0x0000ff });
   cube = new THREE.Mesh(geometry, material);
   cube.position.set(5, 0, 0);
+  // 物体的层级关系。 物体只有和一个正在使用的Camera至少在同一个层时才可见。
+  // 当使用Raycaster进行射线检测的时候此项属性可以用于过滤不参与检测的物体.
+  // 默认值为0
+  cube.layers.set(1); // 相机默认也是0层，所以物体不可见
   scene.add(cube);
 }
 
@@ -98,6 +163,8 @@ function createCamera(x = 0, y = 0, z = 10) {
 function createRenderer() {
   renderer = new THREE.WebGLRenderer({ alpha: true });
   renderer.setSize(innerWidth, innerHeight);
+  // 设置每次渲染不自动清除
+  renderer.autoClear = false;
 }
 
 // 创建渲染函数
@@ -105,7 +172,18 @@ function render() {
   const elapsed = clock.getElapsedTime();
 
   controls && controls.update();
+
+  // 手动清除
+  renderer.clear();
+
+  camera.layers.set(0);
+  composer.render();
+
+  // 清除深度缓冲区片元深度数据
+  renderer.clearDepth();
+  camera.layers.set(1);
   renderer.render(scene, camera);
+
   requestAnimationFrame(render);
 };
 

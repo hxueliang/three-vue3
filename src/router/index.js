4f6d91210ffe8dd4effdd56ae7e98aa981c16f5a
九级浪 <413612464@qq.com>
2023-09-03 17:39:45 +0800

    feat:物体轮廓发光_初始化

--- src/router/index.js
+++ src/router/index.js
@@ -40,7 +40,7 @@ const router = createRouter({
     {
       path: '/baseView',
       name: 'baseView',
-      component: () => import('../views/BaseView30/BaseView.vue')
+      component: () => import('../views/BaseView30/BaseView131.vue')
     },
   ]
 });

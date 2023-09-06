296d2d7a9f3f78705b98a977eccb6ecdad04164f
九级浪 <413612464@qq.com>
2023-09-06 10:53:31 +0800

    feat:元宇宙_canvas绘制纹理_初始化

---
+++ src/views/BaseView34/BaseView139.vue
@@ -0,0 +1,168 @@
+<!-- 139.打造元宇宙 -->
+<!-- canvas绘制纹理 -->
+<template>
+  <div class="container" ref="container"></div>
+</template>
+
+<script setup>
+import { ref, onMounted } from 'vue';
+import gsap from 'gsap';
+import * as THREE from 'three';
+import * as CANNON from 'cannon-es';
+import * as TWEEN from 'three/examples/jsm/libs/tween.module.js';
+import Stats from 'three/examples/jsm/libs/stats.module.js';
+
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import { GUI } from 'three/examples/jsm/libs/lil-gui.module.min.js';
+import { RGBELoader } from 'three/examples/jsm/loaders/RGBELoader';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
+import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader';
+import { KTX2Loader } from 'three/examples/jsm/loaders/KTX2Loader';
+import { DDSLoader } from 'three/examples/jsm/loaders/DDSLoader';
+import { TGALoader } from 'three/examples/jsm/loaders/TGALoader';
+import { EXRLoader } from 'three/examples/jsm/loaders/EXRLoader';
+import { LogLuvLoader } from 'three/examples/jsm/loaders/LogLuvLoader';
+import { RGBMLoader } from 'three/examples/jsm/loaders/RGBMLoader';
+import { Reflector } from "three/examples/jsm/objects/Reflector.js";
+
+let innerWidth = window.innerWidth;
+let innerHeight = window.innerHeight;
+const container = ref(null);
+
+const gui = new GUI();
+const clock = new THREE.Clock();
+const textureLoader = new THREE.TextureLoader();
+
+let scene, camera, renderer, controls, stats;
+
+init();
+
+// 初始化
+function init() {
+  createScene();
+  createCamera(0, 5, 10);
+  createRenderer();
+  createStats();
+  createAxes();
+  window.addEventListener('resize', onWindowResize);
+}
+
+// 业务代码
+function createCode() {
+  createPlane();
+}
+
+// 创建平面
+function createPlane() {
+  const geometry = new THREE.PlaneGeometry(2, 2, 1, 1);
+  const material = new THREE.MeshBasicMaterial({
+    color: 0xffffff,
+    side: THREE.DoubleSide,
+  });
+  const plane = new THREE.Mesh(geometry, material);
+  scene.add(plane);
+}
+
+// 加载环境贴图
+function initEnv() {
+  const hdrLoader = new RGBELoader();
+  hdrLoader.load('./hdr/023.hdr', texture => {
+    texture.mapping = THREE.EquirectangularReflectionMapping;
+    texture.format = THREE.RGBAFormat;
+    scene.background = texture;
+    scene.environment = texture;
+  });
+}
+
+// 创建场景
+function createScene() {
+  scene = new THREE.Scene();
+  // 新增
+  scene.background = new THREE.Color(0x88ccee);
+  // scene.fog = new THREE.Fog(0x88ccee, 0, 50);
+}
+
+// 创建相机
+function createCamera(x = 0, y = 0, z = 10) {
+  camera = new THREE.PerspectiveCamera(70, innerWidth / innerHeight, 0.001, 1000);
+  camera.position.set(x, y, z);
+  scene.add(camera);
+}
+
+// 创建渲染器
+function createRenderer() {
+  renderer = new THREE.WebGLRenderer({ alpha: true });
+  renderer.setSize(innerWidth, innerHeight);
+  // 新增
+  renderer.setPixelRatio(window.devicePixelRatio);
+  renderer.shadowMap.enabled = true;
+  renderer.shadowMap.type = THREE.VSMShadowMap;
+  renderer.outputColorSpace = THREE.SRGBColorSpace;
+  renderer.toneMapping = THREE.ACESFilmicToneMapping;
+}
+
+// 创建渲染函数
+function render() {
+  const elapsed = clock.getElapsedTime();
+
+  stats.update();
+  controls && controls.update();
+  renderer.render(scene, camera);
+  requestAnimationFrame(render);
+};
+
+// 创建性能监控
+function createStats() {
+  stats = new Stats();
+  stats.domElement.style.position = 'absolute';
+  stats.domElement.style.top = '0';
+}
+
+// 创建控制器
+function createControls() {
+  controls = new OrbitControls(camera, container.value);
+  controls.enableDamping = true;
+  controls.dampingFactor = 0.05;
+}
+
+// 添加坐标轴辅助器
+function createAxes(show = true) {
+  if (!show) { return; }
+  const axesHelper = new THREE.AxesHelper(5);
+  scene.add(axesHelper);
+}
+
+// 监听视口变化
+function onWindowResize() {
+  innerWidth = window.innerWidth;
+  innerHeight = window.innerHeight;
+  renderer.setSize(innerWidth, innerHeight);
+  camera.aspect = innerWidth / innerHeight;
+  camera.updateProjectionMatrix();
+}
+
+// 添加画布进DOM
+function appendCanvas() {
+  container.value.appendChild(renderer.domElement);
+}
+
+// 添加stats进DOM
+function appendStats() {
+  container.value.appendChild(stats.domElement);
+}
+
+onMounted(() => {
+  createCode();
+  appendCanvas();
+  appendStats();
+  createControls();
+  render();
+});
+</script>
+
+<style lang="less" scope>
+.container {
+  width: 100vw;
+  height: 100vh;
+}
+</style>

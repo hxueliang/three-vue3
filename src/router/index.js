dc2e36cc1dc724b09513501185295d777ae8e8d3
九级浪 <413612464@qq.com>
2023-09-06 13:48:17 +0800

    feat:元宇宙_视频纹理与canvas纹理结合_初始化

--- src/router/index.js
+++ src/router/index.js
@@ -40,7 +40,7 @@ const router = createRouter({
     {
       path: '/baseView',
       name: 'baseView',
-      component: () => import('../views/BaseView34/BaseView140.vue')
+      component: () => import('../views/BaseView34/BaseView141.vue')
     },
   ]
 });

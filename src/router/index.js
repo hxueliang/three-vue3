9883b2908c6000d03e376d7c7c17b5c9d48bd195
九级浪 <413612464@qq.com>
2023-09-12 19:03:00 +0800

    feat:点对点约束_初始化

--- src/router/index.js
+++ src/router/index.js
@@ -40,7 +40,7 @@ const router = createRouter({
     {
       path: '/baseView',
       name: 'baseView',
-      component: () => import('../views/BaseView37/BaseView157.vue')
+      component: () => import('../views/BaseView37/BaseView158.vue')
     },
   ]
 });

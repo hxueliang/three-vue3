29672f2280d56448844f50fda328a20e97f2508e
九级浪 <413612464@qq.com>
2023-09-12 11:54:12 +0800

    feat:多种形状组合物体_初始化

--- src/router/index.js
+++ src/router/index.js
@@ -40,7 +40,7 @@ const router = createRouter({
     {
       path: '/baseView',
       name: 'baseView',
-      component: () => import('../views/BaseView37/BaseView153.vue')
+      component: () => import('../views/BaseView37/BaseView154.vue')
     },
   ]
 });

ce8dd15e096dc7ac3262c6abc2ae735971ed88d5
九级浪 <413612464@qq.com>
2023-09-12 13:42:16 +0800

    feat:复杂模型的物理碰撞_初始化

--- src/router/index.js
+++ src/router/index.js
@@ -40,7 +40,7 @@ const router = createRouter({
     {
       path: '/baseView',
       name: 'baseView',
-      component: () => import('../views/BaseView37/BaseView154.vue')
+      component: () => import('../views/BaseView37/BaseView155.vue')
     },
   ]
 });

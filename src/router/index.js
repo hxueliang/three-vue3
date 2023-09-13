1fc74587b94d9454584bfed03050121e3af070b1
九级浪 <413612464@qq.com>
2023-09-13 13:19:57 +0800

    feat:铰链约束_初始化

--- src/router/index.js
+++ src/router/index.js
@@ -40,7 +40,7 @@ const router = createRouter({
     {
       path: '/baseView',
       name: 'baseView',
-      component: () => import('../views/BaseView37/BaseView162.vue')
+      component: () => import('../views/BaseView37/BaseView163.vue')
     },
   ]
 });

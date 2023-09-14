f6e768d8d722a63171e83b7848f5016be1989798
九级浪 <413612464@qq.com>
2023-09-14 19:56:39 +0800

    feat:载入模型跟随_初始化

--- src/router/index.js
+++ src/router/index.js
@@ -40,7 +40,7 @@ const router = createRouter({
     {
       path: '/baseView',
       name: 'baseView',
-      component: () => import('../views/BaseView38/BaseView168.vue')
+      component: () => import('../views/BaseView38/BaseView169.vue')
     },
   ]
 });

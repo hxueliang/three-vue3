0ba46e6a9f6a7aff1ff3cadb225e1a5afc9dbc9f
九级浪 <413612464@qq.com>
2023-09-14 19:08:14 +0800

    feat:使用yuka实现路径跟随_初始化

--- src/router/index.js
+++ src/router/index.js
@@ -40,7 +40,7 @@ const router = createRouter({
     {
       path: '/baseView',
       name: 'baseView',
-      component: () => import('../views/BaseView37/BaseView166.vue')
+      component: () => import('../views/BaseView38/BaseView167.vue')
     },
   ]
 });

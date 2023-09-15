88cdfe7f1c63b2189b13fad39c64a7af1fc5a772
九级浪 <413612464@qq.com>
2023-09-15 12:16:04 +0800

    feat:点击修改目标位置_初始化

--- src/router/index.js
+++ src/router/index.js
@@ -40,7 +40,7 @@ const router = createRouter({
     {
       path: '/baseView',
       name: 'baseView',
-      component: () => import('../views/BaseView38/BaseView171.vue')
+      component: () => import('../views/BaseView38/BaseView172.vue')
     },
   ]
 });

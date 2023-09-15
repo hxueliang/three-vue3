50d4c880193d55b803c13dc384c2a82808bb553c
九级浪 <413612464@qq.com>
2023-09-15 20:52:52 +0800

    feat:增加镜面反射_初始化

--- src/router/index.js
+++ src/router/index.js
@@ -40,7 +40,7 @@ const router = createRouter({
     {
       path: '/baseView',
       name: 'baseView',
-      component: () => import('../views/BaseView38/BaseView182.vue')
+      component: () => import('../views/BaseView38/BaseView183.vue')
     },
   ]
 });

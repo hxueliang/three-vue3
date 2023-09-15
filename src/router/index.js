044a8f427219a7152b33e483b41f94dba7ab34ba
九级浪 <413612464@qq.com>
2023-09-15 11:40:18 +0800

    feat:到达目标行为_初始化

--- src/router/index.js
+++ src/router/index.js
@@ -40,7 +40,7 @@ const router = createRouter({
     {
       path: '/baseView',
       name: 'baseView',
-      component: () => import('../views/BaseView38/BaseView170.vue')
+      component: () => import('../views/BaseView38/BaseView171.vue')
     },
   ]
 });

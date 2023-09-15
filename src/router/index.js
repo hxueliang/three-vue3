9e915b4635edb8c9b6c7c26b11032c7751286358
九级浪 <413612464@qq.com>
2023-09-15 12:51:37 +0800

    feat:避开障碍物_初始化

--- src/router/index.js
+++ src/router/index.js
@@ -40,7 +40,7 @@ const router = createRouter({
     {
       path: '/baseView',
       name: 'baseView',
-      component: () => import('../views/BaseView38/BaseView172.vue')
+      component: () => import('../views/BaseView38/BaseView173.vue')
     },
   ]
 });

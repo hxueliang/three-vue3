eff860575496f497e0d9a65146db36927f421a07
九级浪 <413612464@qq.com>
2023-09-21 19:05:02 +0800

    feat:后期发光处理_初始化

--- src/router/index.js
+++ src/router/index.js
@@ -40,7 +40,7 @@ const router = createRouter({
     {
       path: '/baseView',
       name: 'baseView',
-      component: () => import('../views/BaseView41/BaseView188.vue')
+      component: () => import('../views/BaseView41/BaseView189.vue')
     },
   ]
 });

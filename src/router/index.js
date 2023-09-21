7c17261bc2c000cc1cabad21fcd8e79f6c573bd2
九级浪 <413612464@qq.com>
2023-09-21 19:03:28 +0800

    feat:后期边缘处理_初始化

--- src/router/index.js
+++ src/router/index.js
@@ -40,7 +40,7 @@ const router = createRouter({
     {
       path: '/baseView',
       name: 'baseView',
-      component: () => import('../views/BaseView41/BaseView187.vue')
+      component: () => import('../views/BaseView41/BaseView188.vue')
     },
   ]
 });

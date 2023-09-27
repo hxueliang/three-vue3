67329b6bf6626d56af4fea886b16e473ee13c5d2
九级浪 <413612464@qq.com>
2023-09-27 15:52:31 +0800

    feat:科技物理世界_初始化

--- src/router/index.js
+++ src/router/index.js
@@ -40,7 +40,7 @@ const router = createRouter({
     {
       path: '/baseView',
       name: 'baseView',
-      component: () => import('../views/BaseView43/BaseView200.vue')
+      component: () => import('../views/BaseView43/BaseView201.vue')
     },
   ]
 });

2e1a9c99bc45398601ac62fadf1e2ff27327bbd5
九级浪 <413612464@qq.com>
2023-10-03 12:35:33 +0800

    feat:详解查看器viwer，隐藏页面按钮_初始化

--- src/router/index.js
+++ src/router/index.js
@@ -45,7 +45,7 @@ const router = createRouter({
     {
       path: '/cesium',
       name: 'cesium',
-      component: () => import('../views/BaseView32/Cesium01.vue')
+      component: () => import('../views/BaseView32/Cesium02.vue')
     },
   ]
 });

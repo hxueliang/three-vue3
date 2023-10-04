eba169965c72a51dc2267ce275ec358db9dc7116
九级浪 <413612464@qq.com>
2023-10-04 13:04:00 +0800

    feat:添加地形与添加自定义地形_初始化

---
+++ src/views/BaseView32/Cesium03.vue
@@ -0,0 +1,47 @@
+<!-- Cs03.添加地形与添加自定义地形 -->
+<template>
+  <div id="container" ref="container"></div>
+</template>
+
+<script setup>
+import { ref, onMounted } from 'vue';
+import * as Cesium from 'cesium';
+
+import 'cesium/Build/Cesium/Widgets/widgets.css';
+
+import { CESIUM_TOKEN, TDT_TOKEN } from '@/constant/cesium.js';
+
+const container = ref(null);
+
+// 设置cesium静态资源路径
+window.CESIUM_BASE_URL = '/';
+
+// 设置token
+Cesium.Ion.defaultAccessToken = CESIUM_TOKEN;
+
+// 设置cesium默认视角在中国
+Cesium.Camera.DEFAULT_VIEW_RECTANGLE = Cesium.Rectangle.fromDegrees(
+  89.5, // 西边经度
+  20.4, // 南边维度
+  110.4, // 东边经度
+  61.2, // 北边维度
+);
+
+onMounted(() => {
+  const viwer = new Cesium.Viewer('container', {
+  });
+})
+
+</script>
+
+<style lang="less" scope>
+#container {
+  width: 100vw;
+  height: 100vh;
+}
+</style>
+<style>
+body {
+  background-color: #1e1a20;
+}
+</style>

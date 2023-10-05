b75cc8cb66b617af3fb4b0c7c0d9fcc922ef2a2b
九级浪 <413612464@qq.com>
2023-10-05 18:27:05 +0800

    feat:使用primivite创建图像物体_初始化

---
+++ src/views/BaseView32/Cesium11.vue
@@ -0,0 +1,62 @@
+<!-- Cs11.使用primivite创建图像物体 -->
+<template>
+  <div id="container" ref="container"></div>
+</template>
+
+<script setup>
+import { ref, onMounted } from 'vue';
+import * as Cesium from 'cesium';
+
+import 'cesium/Build/Cesium/Widgets/widgets.css';
+
+import { CESIUM_TOKEN, TDT_TOKEN } from '@/constant/cesium.js';
+
+const container = ref(null);
+
+// 设置cesium静态资源路径
+window.CESIUM_BASE_URL = '/';
+
+// 设置token
+Cesium.Ion.defaultAccessToken = CESIUM_TOKEN;
+
+// 设置cesium默认视角在中国
+Cesium.Camera.DEFAULT_VIEW_RECTANGLE = Cesium.Rectangle.fromDegrees(
+  89.5, // 西边经度
+  20.4, // 南边维度
+  110.4, // 东边经度
+  61.2, // 北边维度
+);
+
+onMounted(async () => {
+  const viewer = new Cesium.Viewer('container', {
+  });
+
+  // 使用entities创建矩形
+  const rectangle = viewer.entities.add({
+    rectangle: {
+      coordinates: Cesium.Rectangle.fromDegrees(
+        90, // 西边经度
+        20, // 南边维度
+        110, // 东边经度
+        30 // 北边维度
+      ),
+      material: Cesium.Color.RED.withAlpha(0.5),
+    }
+  });
+
+  viewer.camera.setView(viewer.entities);
+})
+
+</script>
+
+<style lang="less" scope>
+#container {
+  width: 100vw;
+  height: 100vh;
+}
+</style>
+<style>
+body {
+  background-color: #1e1a20;
+}
+</style>

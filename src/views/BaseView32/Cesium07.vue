3b315c4be1041559ba17fcc21ec869e411e74959
九级浪 <413612464@qq.com>
2023-10-05 13:02:13 +0800

    feat:添加物体与3D建筑物_初始化

---
+++ src/views/BaseView32/Cesium07.vue
@@ -0,0 +1,59 @@
+<!-- Cs07.添加物体与3D建筑物 -->
+<template>
+  <div id="container" ref="container"></div>
+</template>
+
+<script setup>
+import { ref, onMounted } from 'vue';
+import * as Cesium from 'cesium';
+
+import 'cesium/Build/Cesium/Widgets/widgets.css';
+
+import { CESIUM_TOKEN, TDT_TOKEN } from '@/constant/cesium.js';
+
+const container = ref(null);
+
+// 设置cesium静态资源路径
+window.CESIUM_BASE_URL = '/';
+
+// 设置token
+Cesium.Ion.defaultAccessToken = CESIUM_TOKEN;
+
+// 设置cesium默认视角在中国
+Cesium.Camera.DEFAULT_VIEW_RECTANGLE = Cesium.Rectangle.fromDegrees(
+  89.5, // 西边经度
+  20.4, // 南边维度
+  110.4, // 东边经度
+  61.2, // 北边维度
+);
+
+onMounted(() => {
+  const viewer = new Cesium.Viewer('container', {
+  });
+
+  // 生成广州塔的位置
+  const position = Cesium.Cartesian3.fromDegrees(113.3191, 23.109, 2000);
+
+  viewer.camera.flyTo({
+    destination: position,
+    orientation: {
+      heading: Cesium.Math.toRadians(0),
+      pitch: Cesium.Math.toRadians(-90),
+      roll: 0,
+    }
+  });
+})
+
+</script>
+
+<style lang="less" scope>
+#container {
+  width: 100vw;
+  height: 100vh;
+}
+</style>
+<style>
+body {
+  background-color: #1e1a20;
+}
+</style>

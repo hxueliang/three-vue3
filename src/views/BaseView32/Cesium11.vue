e8b9ca764834c03638635292742440ba7e3ad060
九级浪 <413612464@qq.com>
2023-10-05 18:27:09 +0800

    feat:使用primivite创建图像物体

--- src/views/BaseView32/Cesium11.vue
+++ src/views/BaseView32/Cesium11.vue
@@ -44,6 +44,40 @@ onMounted(async () => {
     }
   });
 
+  // 使用primivite创建矩形
+  // 1 创建几何体
+  const rectGeometry = new Cesium.RectangleGeometry({
+    rectangle: Cesium.Rectangle.fromDegrees(
+      120, // 西边经度
+      20, // 南边维度
+      140, // 东边经度
+      30 // 北边维度
+    ),
+    height: 0, // 距离表面高度
+    extrudedHeight: 0, // 挤出高度
+    vertexFormat: Cesium.PerInstanceColorAppearance.VERTEX_FORMAT, // 要计算的顶点属性
+  });
+  // 2 创建几何体实例
+  const geoInstance = new Cesium.GeometryInstance({
+    geometry: rectGeometry,
+    attributes: {
+      color: Cesium.ColorGeometryInstanceAttribute.fromColor(
+        Cesium.Color.RED.withAlpha(0.5)
+      )
+    }
+  });
+  // 3 设置外观，即材质
+  const appearance = new Cesium.PerInstanceColorAppearance({
+    flat: true,
+  });
+  // 4 创建图元，即物体
+  const primitive = new Cesium.Primitive({
+    geometryInstances: geoInstance,
+    appearance,
+  });
+  // 5 添加到viewer的场景中
+  viewer.scene.primitives.add(primitive);
+
   viewer.camera.setView(viewer.entities);
 })
 

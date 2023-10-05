fd1a41cce4bba30b3adf4eb59c53a0b173860600
九级浪 <413612464@qq.com>
2023-10-05 18:54:38 +0800

    feat:primivite动态修改颜色_初始化

--- src/router/index.js
+++ src/router/index.js
@@ -45,7 +45,7 @@ const router = createRouter({
     {
       path: '/cesium',
       name: 'cesium',
-      component: () => import('../views/BaseView32/Cesium12.vue')
+      component: () => import('../views/BaseView32/Cesium13.vue')
     },
   ]
 });

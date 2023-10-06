bc558bd2f88e0a3154bdfd63e5c24f036e41d97f
九级浪 <413612464@qq.com>
2023-10-06 18:22:58 +0800

    feat:编写自定义MaterialProperty材质

--- src/views/BaseView32/Cesium22.vue
+++ src/views/BaseView32/Cesium22.vue
@@ -32,6 +32,60 @@ onMounted(async () => {
   const viewer = new Cesium.Viewer('container', {
   });
 
+  class CustomMaterialProperty {
+    constructor() {
+      this.definitionChanged = new Cesium.Event();
+      this.typeName = 'CustomMaterial';
+
+      Cesium.Material._materialCache.addMaterial(this.typeName, {
+        fabric: {
+          type: this.typeName,
+          uniforms: {
+            uTime: 0,
+          },
+          source: `
+            czm_material czm_getMaterial(czm_materialInput materialInput) {
+              czm_material material = czm_getDefaultMaterial(materialInput);
+              material.diffuse = vec3(materialInput.st, uTime);
+              return material;
+            }
+          `,
+        }
+      });
+
+      // 修改uTime，方法二-1：
+      this.params = { uTime: 0 };
+      gsap.to(this.params, {
+        uTime: 1,
+        duration: 1,
+        repeat: -1,
+        yoyo: true,
+        ease: 'linear',
+      });
+    }
+
+    // 获取材质类型
+    getType() {
+      return this.typeName;
+    }
+
+    // 获取value, 本质是要传给uniforms
+    getValue(time, result) {
+      /*
+      // 修改uTime方法一：
+      let t = performance.now() / 1000;
+      t %= 1;
+      result.uTime = t;
+      */
+
+      // 修改uTime方法二-2：
+      result.uTime = this.params.uTime;
+      return result;
+    }
+  }
+
+  const material = new CustomMaterialProperty();
+
   // 使用entities创建矩形
   const rectangle = viewer.entities.add({
     rectangle: {
@@ -41,7 +95,7 @@ onMounted(async () => {
         110, // 东边经度
         30 // 北边维度
       ),
-      material: Cesium.Color.RED.withAlpha(0.5),
+      material,
     }
   });
 })

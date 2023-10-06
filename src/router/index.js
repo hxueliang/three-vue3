f98befc8705d2ec8c61b08ae36e5901529657bc2
九级浪 <413612464@qq.com>
2023-10-06 12:27:46 +0800

    feat:entity关于面的材质_初始化

--- src/router/index.js
+++ src/router/index.js
@@ -45,7 +45,7 @@ const router = createRouter({
     {
       path: '/cesium',
       name: 'cesium',
-      component: () => import('../views/BaseView32/Cesium14.vue')
+      component: () => import('../views/BaseView32/Cesium15.vue')
     },
   ]
 });

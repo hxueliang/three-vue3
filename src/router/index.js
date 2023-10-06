8959ff0213f484cbe5d06cfe5ed10bc46d56bd87
九级浪 <413612464@qq.com>
2023-10-06 18:43:37 +0800

    feat:加载渲染GEOJSON数据_初始化

--- src/router/index.js
+++ src/router/index.js
@@ -45,7 +45,7 @@ const router = createRouter({
     {
       path: '/cesium',
       name: 'cesium',
-      component: () => import('../views/BaseView32/Cesium22.vue')
+      component: () => import('../views/BaseView32/Cesium23.vue')
     },
   ]
 });

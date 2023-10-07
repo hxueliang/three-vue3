c422d65077870ca5f71738183191718dfefe5636
九级浪 <413612464@qq.com>
2023-10-07 13:40:49 +0800

    feat:CZML时间动态图形场景

--- src/views/BaseView32/Cesium29.vue
+++ src/views/BaseView32/Cesium29.vue
@@ -29,7 +29,50 @@ Cesium.Camera.DEFAULT_VIEW_RECTANGLE = Cesium.Rectangle.fromDegrees(
 
 onMounted(async () => {
   const viewer = new Cesium.Viewer('container', {
+    shouldAnimate: true, // 自动开始动画
   });
+
+  // 定义czml数据
+  const czml = [
+    {
+      id: "document",
+      name: "CZML Point - Time Dynamic",
+      version: "1.0",
+    },
+    {
+      id: "point",
+      // 物体在什么时间范围可用
+      availability: "2012-08-04T16:00:00Z/2012-08-04T16:05:00Z",
+      position: {
+        // 设置物体的起始时间
+        epoch: "2012-08-04T16:00:00Z",
+        // 设置了4个维度，1维是时间(单位:秒)，2维是经度，3维是纬度，4维是高度
+        cartographicDegrees: [
+          0, -70, 20, 150000,
+          100, -80, 44, 150000,
+          200, -90, 18, 150000,
+          300, -98, 52, 150000
+        ],
+      },
+      point: {
+        color: {
+          rgba: [255, 255, 255, 128],
+        },
+        outlineColor: {
+          rgba: [255, 0, 0, 128],
+        },
+        outlineWidth: 3,
+        pixelSize: 15,
+      },
+    },
+  ];
+
+  // 加载czml数据
+  const dataSourcePromise = Cesium.CzmlDataSource.load(czml);
+  dataSourcePromise.then(dataSources => {
+    viewer.dataSources.add(dataSources);
+  });
+  viewer.zoomTo(dataSourcePromise);
 })
 
 </script>

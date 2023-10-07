aca207c4af488e00a09fe94ccfd9ee53144843ff
九级浪 <413612464@qq.com>
2023-10-07 13:40:42 +0800

    feat:CZML时间动态图形场景_初始化

--- src/router/index.js
+++ src/router/index.js
@@ -45,7 +45,7 @@ const router = createRouter({
     {
       path: '/cesium',
       name: 'cesium',
-      component: () => import('../views/BaseView32/Cesium28.vue')
+      component: () => import('../views/BaseView32/Cesium29.vue')
     },
   ]
 });

31cf31f2707b0a6b6912d703e9443ed17d4be095
九级浪 <413612464@qq.com>
2023-10-07 15:53:32 +0800

    feat:追踪航班跨洋飞行_初始化

---
+++ src/views/BaseView32/Cesium30.vue
@@ -0,0 +1,45 @@
+<!-- Cs30.追踪航班跨洋飞行 -->
+<template>
+  <div id="container" ref="container"></div>
+</template>
+
+<script setup>
+import { ref, onMounted } from 'vue';
+import * as Cesium from 'cesium';
+
+import 'cesium/Build/Cesium/Widgets/widgets.css';
+
+import { CESIUM_TOKEN, TDT_TOKEN } from '@/constant/cesium.js';
+
+const container = ref(null);
+
+// 设置cesium静态资源路径
+window.CESIUM_BASE_URL = '/';
+
+// 设置token
+Cesium.Ion.defaultAccessToken = CESIUM_TOKEN;
+
+onMounted(async () => {
+  let viewer;
+  try {
+    viewer = new Cesium.Viewer('container', {
+      infoBox: false,
+    });
+  } catch (err) {
+    console.log(err);
+  }
+})
+
+</script>
+
+<style lang="less" scope>
+#container {
+  width: 100vw;
+  height: 100vh;
+}
+</style>
+<style>
+body {
+  background-color: #1e1a20;
+}
+</style>

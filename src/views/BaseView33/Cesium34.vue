307a89a46e5bf69e368a61e048a21d02ce736de6
九级浪 <413612464@qq.com>
2023-10-08 13:14:50 +0800

    feat:制作获取地图经纬度插件_初始化

---
+++ src/views/BaseView33/Cesium34.vue
@@ -0,0 +1,28 @@
+<!-- Cs34.制作获取地图经纬度插件 -->
+<template>
+  <div id="container" ref="container"></div>
+</template>
+
+<script setup>
+import { onMounted } from 'vue';
+import * as Cesium from 'cesium';
+
+import initViewer from './cesium/initViewer';
+
+onMounted(() => {
+  const viewer = initViewer();
+})
+
+</script>
+
+<style lang="less" scope>
+#container {
+  width: 100vw;
+  height: 100vh;
+}
+</style>
+<style>
+body {
+  background-color: #1e1a20;
+}
+</style>
---
+++ src/views/BaseView33/cesium/initViewer.js
@@ -0,0 +1,53 @@
+import * as Cesium from 'cesium';
+import { CESIUM_TOKEN } from '@/constant/cesium.js';
+import 'cesium/Build/Cesium/Widgets/widgets.css';
+
+export default async function initViewer() {
+  // 设置cesium静态资源路径
+  window.CESIUM_BASE_URL = '/';
+
+  // 设置token
+  Cesium.Ion.defaultAccessToken = CESIUM_TOKEN;
+
+  // 设置cesium默认视角在中国
+  Cesium.Camera.DEFAULT_VIEW_RECTANGLE = Cesium.Rectangle.fromDegrees(
+    89.5, // 西边经度
+    20.4, // 南边维度
+    110.4, // 东边经度
+    61.2, // 北边维度
+  );
+
+  try {
+    const viewer = new Cesium.Viewer('container', {
+      infoBox: false,
+      geocoder: false, // 不显示搜索按钮
+      homeButton: false, // 不显示home按钮
+      sceneModePicker: false, // 不显示场景模式选择按钮
+      baseLayerPicker: false, // 不显示图层选择按钮
+      navigationHelpButton: false, // 不显示帮助按钮
+      animation: false, // 不显示动画控制器
+      timeline: false, // 不显示时间轴
+      fullscreenButton: false, // 不显示全屏按钮
+    });
+
+    // 隐藏cesium的logo
+    viewer.cesiumWidget.creditContainer.style.display = 'none';
+
+    // 添加3d建筑
+    const tiles3d = await Cesium.createOsmBuildingsAsync();
+    const osmBuildings = viewer.scene.primitives.add(tiles3d);
+
+    // 广州塔
+    const position = Cesium.Cartesian3.fromDegrees(113.3191, 23.109, 1000);
+
+    viewer.camera.flyTo({
+      destination: position,
+      duration: 2,
+    });
+
+    return viewer;
+  } catch (error) {
+    console.log(error);
+    return { error };
+  }
+}

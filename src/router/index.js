01e6523b79cbca808209521488a3bc1dd2a18b9d
九级浪 <413612464@qq.com>
2023-10-08 15:54:11 +0800

    feat:修改着色器实现酷炫地图颜色_初始化

--- src/router/index.js
+++ src/router/index.js
@@ -45,7 +45,7 @@ const router = createRouter({
     {
       path: '/cesium',
       name: 'cesium',
-      component: () => import('../views/BaseView33/Cesium35.vue')
+      component: () => import('../views/BaseView33/Cesium36.vue')
     },
   ]
 });
---
+++ src/views/BaseView33/Cesium36.vue
@@ -0,0 +1,41 @@
+<!-- Cs36.修改着色器实现酷炫地图颜色 -->
+<template>
+  <div id="container" ref="container"></div>
+</template>
+
+<script setup>
+import { onMounted } from 'vue';
+import * as Cesium from 'cesium';
+import CesiumNavigation from 'cesium-navigation-es6';
+
+import initViewer from './cesium/initViewer';
+import MousePosition from './cesium/MousePosition';
+
+onMounted(async () => {
+  const viewer = await initViewer();
+
+  // 根据鼠标位置生成经纬度值
+  const ousePosition = new MousePosition(viewer);
+
+  // 初始化导航罗盘
+  const navigation = new CesiumNavigation(viewer, {
+    enableCompass: true, // 启用罗盘控件
+    enableZoomControls: true, // 启用缩放控件
+    enableDistanceLegend: true, // 启用距离图例
+    enableCompassOuterRing: true, // 启用指南针外环
+  });
+})
+
+</script>
+
+<style lang="less" scope>
+#container {
+  width: 100vw;
+  height: 100vh;
+}
+</style>
+<style>
+body {
+  background-color: #1e1a20;
+}
+</style>

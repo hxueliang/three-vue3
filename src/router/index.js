e62f51c45a3b66e36551d807a696f329ae19a60c
九级浪 <413612464@qq.com>
2023-10-08 13:47:27 +0800

    feat:指南罗盘与比例尺_初始化

--- src/router/index.js
+++ src/router/index.js
@@ -45,7 +45,7 @@ const router = createRouter({
     {
       path: '/cesium',
       name: 'cesium',
-      component: () => import('../views/BaseView33/Cesium34.vue')
+      component: () => import('../views/BaseView33/Cesium35.vue')
     },
   ]
 });
---
+++ src/views/BaseView33/Cesium35.vue
@@ -0,0 +1,33 @@
+<!-- Cs35.指南罗盘与比例尺 -->
+<!-- https://www.npmjs.com/package/cesium-navigation-es6 -->
+<template>
+  <div id="container" ref="container"></div>
+</template>
+
+<script setup>
+import { onMounted } from 'vue';
+import * as Cesium from 'cesium';
+
+import initViewer from './cesium/initViewer';
+import MousePosition from './cesium/MousePosition';
+
+onMounted(async () => {
+  const viewer = await initViewer();
+
+  // 根据鼠标位置生成经纬度值
+  const ousePosition = new MousePosition(viewer);
+})
+
+</script>
+
+<style lang="less" scope>
+#container {
+  width: 100vw;
+  height: 100vh;
+}
+</style>
+<style>
+body {
+  background-color: #1e1a20;
+}
+</style>

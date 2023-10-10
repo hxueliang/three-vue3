c15fa505175c30eec24bae4f995fbd5c3624043b
九级浪 <413612464@qq.com>
2023-10-10 13:41:56 +0800

    feat:着色器实现彩斑斓雷达特效_初始化

--- src/router/index.js
+++ src/router/index.js
@@ -45,7 +45,7 @@ const router = createRouter({
     {
       path: '/cesium',
       name: 'cesium',
-      component: () => import('../views/BaseView33/Cesium40.vue')
+      component: () => import('../views/BaseView33/Cesium41.vue')
     },
   ]
 });

ee58b5e73e471f8516cf80e22b0e34ea4a47ed25
九级浪 <413612464@qq.com>
2023-10-14 16:04:07 +0800

    feat:假阴影

--- src/views/BaseView6/56.3.假阴影.vue
+++ src/views/BaseView6/56.3.假阴影.vue
@@ -37,6 +37,8 @@ gltfLoader.setDRACOLoader(dracoLoader);
 
 let scene, camera, renderer, controls;
 
+const sphereShadowBases = [];
+
 init();
 
 // 初始化
@@ -56,15 +58,85 @@ function init() {
 // 业务代码
 function createCode() {
   // 地面
-  const planeSize = 40;
-  const planeGeo = new THREE.PlaneGeometry(planeSize, planeSize);
-  const planeMat = new THREE.MeshBasicMaterial({
+  const floorSize = 40;
+  const floorGeo = new THREE.PlaneGeometry(floorSize, floorSize);
+  const floorMat = new THREE.MeshBasicMaterial({
     color: 0xaaaaaa,
     side: THREE.DoubleSide,
   });
-  const mesh = new THREE.Mesh(planeGeo, planeMat);
-  mesh.rotation.x = Math.PI * -.5;
-  scene.add(mesh);
+  const floor = new THREE.Mesh(floorGeo, floorMat);
+  floor.rotation.x = Math.PI * -.5;
+  scene.add(floor);
+
+  // 灯光1
+  const color = 0xffffff;
+  const intensity1 = 1;
+  const light = new THREE.DirectionalLight(color, intensity1);
+  light.position.set(0, 10, 5);
+  light.target.position.set(-5, 0, 0);
+  scene.add(light);
+  scene.add(light.target);
+
+  // 灯光2
+  const skyColor = 0xb1e1ff; // light blue
+  const groundColor = 0xb97a20; // brownish orange
+  const intensity2 = 2;
+  const hemisphereLightlight = new THREE.HemisphereLight(skyColor, groundColor, intensity2);
+  scene.add(hemisphereLightlight);
+
+  // 球
+  const sphereRadius = 1;
+  const sphereWidthDivisions = 32;
+  const sphereHeightDivisions = 16;
+  const sphereGeo = new THREE.SphereGeometry(
+    sphereRadius,
+    sphereWidthDivisions,
+    sphereHeightDivisions
+  );
+  // 球阴影
+  const planeSize = 1;
+  const shadowGeo = new THREE.PlaneGeometry(planeSize, planeSize);
+  // const shadowTexture = loader.load("./images/roundshadow.png");
+  const numSpheres = 15;
+  for (let i = 0; i < numSpheres; ++i) {
+    // 为阴影和球体创建一个基底
+    // 方便一起移动
+    const base = new THREE.Object3D();
+    scene.add(base);
+
+    // 给baseloader添加阴影
+    // 注意:我们为每个球体制作了一个新的材质
+    // 所以我们可以设置球体的材质透明度
+    // 分别。
+    const shadowMat = new THREE.MeshBasicMaterial({
+      color: 0x666666,
+      // map: shadowTexture, // todo
+      transparent: true, // 这样我们就能看到地面了
+      depthWrite: false, // 我们不需要排序
+    });
+    const shadowMesh = new THREE.Mesh(shadowGeo, shadowMat);
+    shadowMesh.position.y = 0.001; // 离地面稍微高一点
+    shadowMesh.rotation.x = Math.PI * -0.5;
+    const shadowSize = sphereRadius * 4;
+    shadowMesh.scale.set(shadowSize, shadowSize, shadowSize);
+    base.add(shadowMesh);
+
+    // 将球体添加到基座
+    const u = i / numSpheres; // 当我们迭代球体时，从0到1。
+    const sphereMat = new THREE.MeshPhongMaterial();
+    sphereMat.color.setHSL(u, 1, 0.75);
+    const sphereMesh = new THREE.Mesh(sphereGeo, sphereMat);
+    sphereMesh.position.set(0, sphereRadius + 2, 0);
+    base.add(sphereMesh);
+
+    // 记住所有的球加上y的位置
+    sphereShadowBases.push({
+      base,
+      sphereMesh,
+      shadowMesh,
+      y: sphereMesh.position.y,
+    });
+  }
 }
 
 // 创建场景
@@ -87,8 +159,29 @@ function createRenderer() {
 }
 
 // 创建渲染函数
-function render() {
-  const elapsed = clock.getElapsedTime();
+function render(time) {
+  time *= 0.001;  // 转换为秒
+
+  sphereShadowBases.forEach((sphereShadowBase, ndx) => {
+    const { base, sphereMesh, shadowMesh, y } = sphereShadowBase;
+
+    // U是一个从0到1的值，当我们迭代球面时
+    const u = ndx / sphereShadowBases.length;
+
+    // 计算基座的位置它会移动
+    // 球体和它的阴影
+    const speed = time * .2;
+    const angle = speed + u * Math.PI * 2 * (ndx % 1 ? 1 : -1);
+    const radius = Math.sin(speed - ndx) * 10;
+    base.position.set(Math.cos(angle) * radius, 0, Math.sin(angle) * radius);
+
+    // yOff 是一个从0到1的值
+    const yOff = Math.abs(Math.sin(time * 2 + ndx));
+    // 上下移动球体
+    sphereMesh.position.y = y + THREE.MathUtils.lerp(-2, 2, yOff);
+    // 随着球体上升，阴影逐渐消失
+    shadowMesh.material.opacity = THREE.MathUtils.lerp(1, .25, yOff);
+  });
 
   controls && controls.update();
   renderer.render(scene, camera);

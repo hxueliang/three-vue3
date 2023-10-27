9377ce0f9e810d1eb8b3cb8bbe678df7ff70c8fe
九级浪 <413612464@qq.com>
2023-10-27 14:57:00 +0800

    feat:四元数表示两个向量旋转

--- src/views-high/02四元数/03四元数表示两个向量旋转.vue
+++ src/views-high/02四元数/03四元数表示两个向量旋转.vue
@@ -65,6 +65,23 @@ function createCode() {
     fly.position.set(1, 1, 0); // 相对世界坐标系坐标原点偏移
     const axesHelper = new THREE.AxesHelper(5 / scale);
     fly.add(axesHelper); // 用一个坐标轴可视化模型的局部坐标系(本地坐标系)
+
+    // 飞机初始姿态飞行方向
+    const a = new THREE.Vector3(0, 0, 1);
+    // 飞机姿态绕自身坐标原点旋转到b指向的方向
+    const b = new THREE.Vector3(-1, -1, -1).normalize();
+
+    // 可视化飞机方向
+    const O = fly.position.clone();//飞机位置坐标箭头起点
+    model.add(new THREE.ArrowHelper(a, O, 3, 0xff0000));
+    model.add(new THREE.ArrowHelper(b, O, 3, 0x00ff00));
+
+    // a旋转到b构成的四元数
+    const quaternion = new THREE.Quaternion();
+    // 注意两个参数的顺序
+    quaternion.setFromUnitVectors(a, b);
+    // quaternion表示的是变化过程，在原来基础上乘以quaternion即可
+    fly.quaternion.multiply(quaternion);
   });
 
 }
